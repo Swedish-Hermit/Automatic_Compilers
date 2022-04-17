cd34636627cdf03bc2ebe22b57594cd45c3ba79e
Robin Vörde <rmtintranet@gmail.com>
2022-04-17 10:27:50 +0200

    Cleanup and added another if statement for installation of kernel

--- kernel.bash
+++ kernel.bash
@@ -1,41 +1,28 @@
 #!/bin/bash
-
+# If you pass flag INSTALL then run installation for kernel after compile!
+if ["$INSTALL" == true ] ; then
+    echo Installing kernel after compile!
+fi
+# Variable INSTALL
+INSTALL= make modules_install && make install
 # Error if script not executed as root
 # sudo !! # will run last entry in history with sudo
-
 [[ "$(id -u)" == 0 ]] || { echo "Run: sudo !!" >&2 ; exit 1 ; }
-
 ### Create directory for the sources
 mkdir kernel
 cd kernel
-
-### Dependiances
+# Dependiances
 apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison -y 
-
-### Look for latest file and download it 
+# Look for latest file and download it 
 #wget "$(curl "https://kernel.org" | grep -A3 "<td>mainline:</td>" | grep ".tar.gz" | cut -d\" -f2)" -O latest.tar.gz
 wget "$(curl "https://kernel.org/" | grep -A3 "<td>mainline:</td>" | grep ".tar.gz" | cut -d\" -f2)" -O latest.tar.gz
-
 tar -xvf latest.tar.gz
 cd linux*
-
-### Copy existing config over to new kernel
+# Copy existing config over to new kernel
 cp -v /boot/config-$(uname -r) .config
-
-### Compile menuconfig
+# Compile menuconfig
 make menuconfig
-
-### Compile kernel
+# Compile kernel
 make -j4
-
-### install modules
-make modules_install
-
-### install kernel
-make install
-
-### enable initramfs on boot
-#update-initramfs -c -k all
-
-### update grub
-#update-grub
+# Install modules
+$INSTALL
