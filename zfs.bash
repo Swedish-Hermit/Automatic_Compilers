4cff882974e7df3ea256876d1ab14079c7d4a1fa
Robin Holer <robin@home.robinsmediateam.dev>
2022-05-12 13:18:44 +0200

    fixed some problems in if statement

--- zfs.bash
+++ zfs.bash
@@ -2,7 +2,7 @@
 Dependencies='git build-essential autoconf automake libtool gawk alien fakeroot dkms libblkid-dev uuid-dev libudev-dev libssl-dev zlib1g-dev libaio-dev libattr1-dev libelf-dev 'linux-headers-$(uname -r)' python3 python3-dev python3-setuptools python3-cffi libffi-dev python3-packaging git libcurl4-openssl-dev'
 [[ "$(id -u)" == 0 ]] || { echo "Run: sudo !!" >&2 ; exit 1 ; }
 apt update && apt install $Dependencies
-if [[ -d $HOME/zfs == true ]]; then
+if [[ -d $HOME/zfs ]]; then
     cd $HOME
     echo 'running git pull then compile'
     cd ./zfs && git pull 
