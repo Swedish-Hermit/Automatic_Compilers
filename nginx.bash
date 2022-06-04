0aa4747a7924ab7e463728b05c8c91455628811d
Robin Holer <robin@home.robinsmediateam.dev>
2022-06-04 14:59:29 +0200

    added some more modules

--- nginx.bash
+++ nginx.bash
@@ -18,6 +18,8 @@ sudo apt-get update \
         git \
         make \
         tar \
+        libpcre3 \
+        libpcre3-dev
 
 ### Look for latest file and download it 
 NGINX_VER="$(curl -s 'http://nginx.org/download/' | grep -oP 'href="nginx-\K[0-9]+\.[0-9]+\.[0-9]+' | sort -t. -rn -k1,1 -k2,2 -k3,3 | head -1)"
@@ -27,7 +29,8 @@ tar -xvf $NGINX_VER.tar.gz
 
 #clone fancyindex git repo
 git clone https://github.com/aperezdc/ngx-fancyindex.git ./ngx-fancyindex
-
+#wget https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.40/pcre2-10.40.tar.gz && tar xvf
+#wget https://www.openssl.org/source/openssl-3.0.3.tar.gz && tar xvf 
 #cd into nginx folder
 cd nginx
 cd nginx-$NGINX_VER
@@ -46,6 +49,16 @@ cd nginx-$NGINX_VER
                 --with-http_random_index_module \
                 --with-http_stub_status_module \
                 --with-http_sub_module \
+                --with-stream_ssl_module \
+                --with-stream \
+#                --with-openssl=../openssl-3.0.3 \
+                --with-http_mp4_module \
+                --with-http_sub_module \
+                --with-http_dav_module \
+                --with-http_flv_module \
+                --with-http_ssl_module \
+#                --with-pcre=../pcre2-10.40 \
+                --with-http_gzip_static_module \
                 --without-http_rewrite_module \
                 --add-module=../ngx-fancyindex \
                 --without-http_uwsgi_module \
