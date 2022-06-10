581fa568b27801ed25dd249cd4a4ae54d7d3529f
Robin Holer <robin@home.robinsmediateam.dev>
2022-06-10 23:23:54 +0200

    changed extension names, added some echo

--- zfs.sh
+++ zfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Dependencies='git build-essential autoconf automake libtool gawk alien fakeroot dkms libblkid-dev uuid-dev libudev-dev libssl-dev zlib1g-dev libaio-dev libattr1-dev libelf-dev 'linux-headers-$(uname -r)' python3 python3-dev python3-setuptools python3-cffi libffi-dev python3-packaging git libcurl4-openssl-dev'
-[[ "$(id -u)" == 0 ]] || { echo "Run: sudo !!" >&2 ; exit 1 ; }
+[[ "$(id -u)" == 0 ]] || { echo "Run: sudo" >&2 ; exit 1 ; }
 apt update && apt install $Dependencies
 if [[ -d $HOME/zfs ]]; then
     cd $HOME/zfs && git pull
