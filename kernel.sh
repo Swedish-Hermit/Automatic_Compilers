69da7db59700d3f41cd0e9415327d106781ee3b6
Robin Vörde <rmtintranet@gmail.com>
2022-11-05 14:11:50 +0100

    revamped kernel script

--- kernel.sh
+++ kernel.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 Dependencies='git fakeroot build-essential make ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison curl wget'
-# If you pass flag INSTALL then run installation for kernel after compile!
-if [ "install" == "true" ]; then
-    echo 'Installing kernel after compile!'
     # Error if script not executed as root
     # sudo !! # will run last entry in history with sudo
     [[ "$(id -u)" == 0 ]] || { echo "Run: sudo !!" >&2 ; exit 1 ; }
     # Create directory for the sources
-    mkdir kernel
+    mkdir kernel;
     cd kernel
+    # Remove existing installs of the kernel
+    rm latest.tar.gz; rm -r linux*;
     # Dependencies
     apt-get install -y $Dependencies 
     # Look for latest file and download it 
@@ -20,24 +19,6 @@ if [ "install" == "true" ]; then
     cp -v /boot/config-$(uname -r) .config
     # Make menu config and display it for changes
     make menuconfig
-    # Compile kernel and install kernel
-    make -j5 && make modules_install && make install
-    # Install modules with the flag INSTALL
+    # Compile kernel for distribution
+    make -j $(nproc) bindeb-pkg
     exit 0
-else
-    echo 'not installing kernel after Compile!'
-    ### Create directory for the sources
-    mkdir kernel
-    cd kernel
-    # Dependencies
-    apt-get install -y $Dependencies
-    # Look for latest file and download it 
-    wget "$(curl "https://kernel.org/" | grep -A3 "<td>mainline:</td>" | grep ".tar.gz" | cut -d\" -f2)" -O latest.tar.gz
-    tar -xvf latest.tar.gz
-    cd ./linux*
-    # Compile kernel
-    make -j6
-    # Install modules with the flag INSTALL
-    echo Install the modules and kernel with "install"
-    exit 0
-fi
